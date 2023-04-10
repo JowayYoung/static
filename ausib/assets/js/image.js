b77450dd3f75d4698e6c7b79f8ff4a2fbf0f5340
yangzewei <young.joway@aliyun.com>
2023-04-10 20:00:31 +0800

    remove

--- ausib/assets/js/image.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$("img.lazyimg").lazyload({
-		data_attribute: "src",
-		effect: "fadeIn",
-		effect_speed: 1000,
-		placeholder: "assets/img/lazyimg.jpg",
-		threshold: -200
-	});
-});
